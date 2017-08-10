c3f4632d8e8793a52870190a46b2d7088e4b14a0
Brian Lusina <lusinabrian@gmail.com>
2017-08-10 09:24:59 +0300

    Restructure project

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../images/svg/logo.svg';
+import '../styles/_app.scss';
 
 class App extends Component {
   render() {
