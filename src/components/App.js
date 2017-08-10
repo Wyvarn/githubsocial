04024836e4fa12535d9fac5f89fa14b46ffab377
Brian Lusina <lusinabrian@gmail.com>
2017-08-10 09:31:31 +0300

    Create constants file, update styles and add container

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,41 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from '../images/svg/logo.svg';
-import '../styles/_app.scss';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h2>Github Social</h2>
+                </div>
+
+                <div className="container">
+                    <div className="header">
+                        <h2>Who to follow</h2><a href="#" className="refresh">Refresh</a>
+                    </div>
+                    <ul className="suggestions">
+                        <li className="suggestion1">
+                            <img />
+                            <a href="#" target="_blank" className="username">this will not be displayed</a>
+                            <a href="#" className="close close1">x</a>
+                        </li>
+                        <li className="suggestion2">
+                            <img />
+                            <a href="#" target="_blank" className="username">neither this</a>
+                            <a href="#" className="close close2">x</a>
+                        </li>
+                        <li className="suggestion3">
+                            <img />
+                            <a href="#" target="_blank" className="username">nor this</a>
+                            <a href="#" className="close close3">x</a>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/constants/constants.js
@@ -0,0 +1,4 @@
+/**
+ * @author lusinabrian on 10/08/17.
+ * @notes: Commonly used constants in application
+ */
